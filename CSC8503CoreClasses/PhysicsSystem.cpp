6bd8e0cb7bbb47e5abf5f39b3f5ee9b6f2ad9f2a
Aiman Khan <c2055955@newcastle.ac.uk>
2022-11-27 18:27:23 +0000

    added angular accel and vel

--- CSC8503CoreClasses/PhysicsSystem.cpp
+++ CSC8503CoreClasses/PhysicsSystem.cpp
@@ -252,6 +252,14 @@ void PhysicsSystem::IntegrateAccel(float dt) {
 		}
 		linearVel += accel * dt; // integrate accel!
 		object->SetLinearVelocity(linearVel);
+
+		// Angular stuff
+		Vector3 torque = object->GetTorque();
+		Vector3 angVel = object->GetAngularVelocity();
+		object->UpdateInertiaTensor(); // update tensor vs orientation
+		Vector3 angAccel = object->GetInertiaTensor() * torque;
+		angVel += angAccel * dt; // integrate angular accel!
+		object->SetAngularVelocity(angVel);
 	}
 }
 
@@ -280,6 +288,18 @@ void PhysicsSystem::IntegrateVelocity(float dt) {
 		// Linear Damping
 		linearVel = linearVel * frameLinearDamping;
 		object->SetLinearVelocity(linearVel);	
+
+		// Orientation Stuff
+		Quaternion orientation = transform.GetOrientation();
+		Vector3 angVel = object->GetAngularVelocity();
+		orientation = orientation + (Quaternion(angVel * dt * 0.5f, 0.0f) * orientation);
+		orientation.Normalise();
+		transform.SetOrientation(orientation);
+		
+		//Damp the angular velocity too
+		float frameAngularDamping = 1.0f - (0.4f * dt);
+		angVel = angVel * frameAngularDamping;
+		object->SetAngularVelocity(angVel);
 	}
 }
 
